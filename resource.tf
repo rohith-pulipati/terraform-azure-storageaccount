521ab6d68cca30804e8ce5db95decc4a33b52efb
rohith-pulipati <rohithkumar.pulipati@gmail.com>
2024-09-04 11:01:34 +0530

    Add files via upload

---
+++ resource.tf
@@ -0,0 +1,14 @@
+module "storagemodule" {
+  source = "./modules/storageaccount"
+  storageaccountname = "rohithp1storage"
+  resourcegroupname = "TerraformRG"
+}
+
+output "storageidoutput" {
+  value = module.storagemodule.storageid
+  description = "The id of the storage account"
+}
+
+output "Storagetier" {
+  value = module.storagemodule.StorageTier
+}
